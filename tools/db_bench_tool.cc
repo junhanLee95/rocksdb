419864d63eda95ad097b72987f76bbeed27bc023
dohyun4212 <ehgus421210@gmail.com>
2021-01-11 14:59:47 +0900

    Fix the trace file name

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -6100,7 +6100,7 @@ void VerifyDBFromDB(std::string& truth_db_name) {
   }
 };
 
-int db_bench_tool(int argc, char** argv, rocksdb::Env* bluefs_env) {
+int db_bench_tool(int argc, char** argv) {
   rocksdb::port::InstallStackTraceHandler();
   static bool initialized = false;
   if (!initialized) {
@@ -6210,10 +6210,6 @@ int db_bench_tool(int argc, char** argv, rocksdb::Env* bluefs_env) {
     // When both are set then FLAGS_stats_interval determines the frequency
     // at which the timer is checked for FLAGS_stats_interval_seconds
     FLAGS_stats_interval = 1000;
-  }
-
-  if (bluefs_env) {
-    FLAGS_env = bluefs_env;
   }
 
   rocksdb::Benchmark benchmark;
