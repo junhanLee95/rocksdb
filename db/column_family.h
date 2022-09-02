bb97222e29dd1506ece0ad85a0ed052eb20b2b9a
dohyun4212 <dohyun4212@gmail.com>
2022-09-02 13:58:10 +0900

    Fix bug: include partition tree class error

--- db/column_family.h
+++ db/column_family.h
@@ -46,6 +46,7 @@ class LogBuffer;
 class InstrumentedMutex;
 class InstrumentedMutexLock;
 struct SuperVersionContext;
+class PartitionTree;
 
 extern const double kIncSlowdownRatio;
 
@@ -620,7 +621,7 @@ class ColumnFamilySet {
   std::unordered_map<uint32_t, ColumnFamilyData*> column_family_data_;
   int comp_smallest_key (ColumnFamilyData* c1, ColumnFamilyData* c2) { return c1->GetSmallestKey().compare(c2->GetSmallestKey()); };
   std::vector<ColumnFamilyData*> logical_column_family_data_;
-  PartitionTree partition_tree_;
+  PartitionTree* partition_tree_;
 
   uint32_t max_column_family_;
   ColumnFamilyData* dummy_cfd_;
