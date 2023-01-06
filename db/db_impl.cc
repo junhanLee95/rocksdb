718be4a7aa4ed53299e375b498fae82a1e9853ea
seungho <limgb2001@gmail.com>
2023-01-06 15:43:58 +0900

    again test

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -2603,7 +2603,6 @@ Iterator* DBImpl::NewIterator(const ReadOptions& read_options,
     auto snapshot = read_options.snapshot != nullptr
                         ? read_options.snapshot->GetSequenceNumber()
                         : versions_->LastSequence();
-    //test
 	result = NewIteratorImpl(read_options, cfd, snapshot, read_callback);
   }
   return result;
